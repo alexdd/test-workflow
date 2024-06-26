66e667679f230133b41f200d24a87812f2faa307
alexdusel <alex.dusel@caditeurope.com>
2024-06-26 15:53:27 +0200

    testing

--- obfuscate.py
+++ obfuscate.py
@@ -2,6 +2,6 @@ import sys
 
 pwd = sys.argv[1:][0]
 if pwd == "1234":
-    print("PWD successfully evaluated!")
+    print("PWD successfully evaluated, encrypted and injected!")
 else:
     print("Could not evaluate PWD!")
